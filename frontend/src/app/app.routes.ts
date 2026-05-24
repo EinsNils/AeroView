3b370e6fe76c3c5464761d7a90a6f9109f9343a9
Nils Zentgraf <nils.zentgraf@aol.com>
2026-05-24 12:55:43 +0200

    feat: set up Angular routing and initial environment configurations

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'map',      loadChildren: () => import('./map/map.routes').then(m => m.MAP_ROUTES) },
+  { path: 'flights',  loadChildren: () => import('./flights/flights.routes').then(m => m.FLIGHT_ROUTES) },
+  { path: 'airports', loadChildren: () => import('./airports/airports.routes').then(m => m.AIRPORT_ROUTES) },
+  { path: 'auth',     loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES) },
+  { path: '',         redirectTo: 'map', pathMatch: 'full' },
+];
